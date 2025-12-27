9947da54a96ad8eb96e97b4a8f6ccca711fe7615
cs_xuhuan <cs_xuhuan@163.com>
2025-12-28 03:06:56 +0800

    chore: introduce layered package structure

---
+++ src/main/java/com/csxuhuan/gelatoni/application/GelatoniApplication.java
@@ -0,0 +1,11 @@
+package com.csxuhuan.gelatoni.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelatoniApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GelatoniApplication.class, args);
+    }
+}
